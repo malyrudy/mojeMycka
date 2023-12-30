37f5fcd959a9b53f5fef3a2fdfa45405ebd209e5
malyrudy <98644282+malyrudy@users.noreply.github.com>
2023-12-30 14:59:08 +0100

    pridan kod pro navigaci

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 const navButtonOpen = document.querySelector(".hamburger-menu-icon");
+const navbarMenu = document.querySelector(".navbar-ul");
+const navbarMenuItems = document.querySelectorAll(".navbar-ul li");
 
-navButtonOpen.addEventListener("click", () => {console.log("Bylo stisknuto na tlačítko.")})
+function activeMenu() {
+    navbarMenu.classList.toggle("active");
+}
+
+navButtonOpen.addEventListener("click", () => {
+    console.log("Ahoj");
+    activeMenu()
+})
+
+navbarMenuItems.forEach(item => {
+    item.addEventListener("click", activeMenu);
+});
