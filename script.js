9eea7fc17f1a61a2ac4675069fa21287abe816af
malyrudy <98644282+malyrudy@users.noreply.github.com>
2023-12-30 15:54:20 +0100

    zmena svg

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const navButtonOpen = document.querySelector(".hamburger-menu-icon");
+const hamburgerIcon = document.querySelector(".hamburger-menu-icon");
 const navbarMenu = document.querySelector(".navbar-ul");
 const navbarMenuItems = document.querySelectorAll(".navbar-ul li");
 
@@ -6,11 +6,16 @@ function activeMenu() {
     navbarMenu.classList.toggle("active");
 }
 
-navButtonOpen.addEventListener("click", () => {
-    console.log("Ahoj");
-    activeMenu()
+hamburgerIcon.addEventListener("click", () => {
+    document.querySelector('.line1').classList.toggle('active');
+    document.querySelector('.line2').classList.toggle('active');
+    activeMenu();
 })
 
 navbarMenuItems.forEach(item => {
-    item.addEventListener("click", activeMenu);
+    item.addEventListener("click", function(){
+        activeMenu();
+        document.querySelector('.line1').classList.toggle('active');
+        document.querySelector('.line2').classList.toggle('active');
+    })
 });
