7e4f3ca322e530c01dd8270ad2d269b6dd902458
malyrudy <98644282+malyrudy@users.noreply.github.com>
2024-01-16 20:13:55 +0100

    pridana sekce darkovy poukaz

--- script.js
+++ script.js
@@ -32,3 +32,27 @@ ScrollReveal().reveal('.reviews-list', { origin: "top" });
 ScrollReveal().reveal('.about-us-content', { origin: "bottom" });
 ScrollReveal().reveal('#home .text-content, .my-contact-info', { origin: "left" });
 ScrollReveal().reveal('.image-content, .services-item, form', { origin: "right" });
+
+// Předpokládáme, že máte cesty k obrázkům ve formě pole nebo můžete je definovat individuálně
+var imagePaths = ['./images/dárkový_poukaz1.jpg', './images/dárkový_poukaz2.jpg', './images/dárkový_poukaz3.jpg', './images/dárkový_poukaz4.jpg'];
+
+// Pro každý odkaz vytvořte a přidejte do stránky
+for (var i = 0; i < imagePaths.length; i++) {
+    createAndAppendDownloadLink(imagePaths[i], 'dárkový_poukaz' + (i + 1) + '.jpg', i + 1);
+}
+
+function createAndAppendDownloadLink(imagePath, downloadFileName, index) {
+    // Vytvoření odkazu na stáhnutí
+    var link = document.createElement('a');
+    link.href = imagePath;
+    link.download = downloadFileName;
+
+    // Přidání odkazu do kontejneru
+    document.getElementById('download-container').appendChild(link);
+    
+    // Přidejte posluchač událostí na tlačítko pro potvrzení kliknutí
+    document.querySelector('.darkovy-poukaz-item:nth-child(' + index + ') .btn').addEventListener('click', function() {
+        // Simulujte kliknutí na odkaz
+        link.click();
+    });
+}
